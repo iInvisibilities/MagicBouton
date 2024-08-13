6d7e47be9d121dcb81f081247c6c89b07736caf5
BuildTools <unconfigured@null.spigotmc.org>
2024-08-13 16:27:41 +0100

    fix path try #fml

--- src/routes/getcss/+server.ts
+++ src/routes/getcss/+server.ts
@@ -1,11 +1,14 @@
 import fs from 'fs';
+//import MagicBoutonFile from '$lib/MagicBouton_b0.1.css';
 
 export async function GET({ params, url }) {
+	//console.log(MagicBoutonFile);
+
 	let fromCol = url.searchParams.get('from');
 	let viaCol = url.searchParams.get('via');
 	let toCol = url.searchParams.get('to');
 
-	let fileContent = fs.readFileSync('static/MagicBouton_b0.1.css', 'utf-8');
+	let fileContent = fs.readFileSync('src/lib/MagicBouton_b0.1.css', 'utf-8');
 
 	fileContent = fileContent.replace('#fa5477', fromCol);
 	fileContent = fileContent.replace('#ef4b4b', viaCol);
